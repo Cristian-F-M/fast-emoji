32709bde764dfc2e274c625f3e1cbd35d9f6209d
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-04-05 16:13:03 -0500

    Add keys to suppres and keys for control and shift keys

---
+++ src/constants/main.py
@@ -0,0 +1,10 @@
+from src.data import vk_from_string
+
+KEYS_TO_SUPPRES = {
+    vk_from_string[k]
+    for k in ("VK_RETURN", "VK_LEFT", "VK_UP", "VK_RIGHT", "VK_DOWN", "VK_ESCAPE")
+}
+
+
+CTRL_KEYS = {"VK_LCONTROL", "VK_RCONTROL", "VK_CONTROL"}
+SHIFT_KEYS = {"VK_LSHIFT", "VK_RSHIFT", "VK_SHIFT"}
