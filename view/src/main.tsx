45cb589df6fc0ba7a7a4215d1e9cd604dfb386aa
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-04-09 17:25:48 -0500

    Add routes for main app

--- view/src/main.tsx
+++ view/src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from '@/components/App.tsx'
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router'
 import './global.css'
+import { About, HowToUse, Settings } from '@/components/main-view/'
+import SideMenu from '@/components/SideMenu'
+import { Main } from '@/windows/Main'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<SideMenu />}>
+					<Route index element={<Main />} />
+					<Route path="how-to-use" element={<HowToUse />} />
+					<Route path="settings" element={<Settings />} />
+					<Route path="about" element={<About />} />
+				</Route>
+
+				<Route path="*" element={<h1>404 global</h1>} />
+			</Routes>
+		</BrowserRouter>
 	</StrictMode>
 )
