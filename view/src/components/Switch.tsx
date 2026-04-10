99c01d7840cc02c806ad4c388eb817325cced484
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-04-10 11:21:02 -0500

    Add switch component

---
+++ view/src/components/Switch.tsx
@@ -0,0 +1,35 @@
+interface SwitchProps extends React.HTMLAttributes<HTMLInputElement> {
+	id: string
+	name: string
+	value: boolean
+	onValueChange: (value: boolean) => void
+	labelProps?: React.HTMLAttributes<HTMLLabelElement>
+}
+
+export function Switch({
+	className,
+	id,
+	name,
+	value,
+	onValueChange,
+	...props
+}: SwitchProps) {
+	return (
+		<label htmlFor={id} className="cursor-pointer flex items-center">
+			<div className="relative w-12 h-6">
+				<input
+					id={id}
+					name={name}
+					type="checkbox"
+					checked={value}
+					onChange={(e) => onValueChange(e.target.checked)}
+					className="sr-only peer"
+					{...props}
+				/>
+
+				<div className="w-full h-full bg-surface rounded-full peer-checked:bg-primary/20 transition-colors" />
+				<div className="absolute top-1/2 left-1 -translate-y-1/2 w-4 h-4 rounded-full bg-primary/30 peer-checked:translate-x-6 peer-checked:bg-primary transition-all duration-200 ease-in-out" />
+			</div>
+		</label>
+	)
+}
