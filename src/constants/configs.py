577f0d41c0c79e25910a3ad28aa19f7460c9b3e5
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-04-11 15:34:52 -0500

    Add more configs

--- src/constants/configs.py
+++ src/constants/configs.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from typing import Any
 import darkdetect  # type: ignore
 
 is_dark: bool = darkdetect.isDark() or False
@@ -9,4 +10,10 @@ CONFIGS_DIR_PATH = Path(APP_DATA_PATH, "Fast Emoji")
 CONFIGS_PATH = Path(CONFIGS_DIR_PATH, "config.init")
 
 
-DEFAULT_CONFIGS = {"app": {"theme": "dark" if is_dark else "light"}}
+DEFAULT_CONFIGS: dict[str, Any] = {
+    "app": {
+        "theme": "dark" if is_dark else "light",
+        "launch_at_startup": True,
+        "run_in_background": True,
+    }
+}
